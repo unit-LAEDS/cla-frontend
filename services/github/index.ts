959eb49c5a2258eb03a4dc87d4b4f6df6d1d3d1d
Nathan Gomes <nahtann@outlook.com>
2022-07-23 22:40:10 -0300

    🚧 work in progress: profile page

---
+++ services/github/index.ts
@@ -0,0 +1,44 @@
+export interface GitHubInfoInterface {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  name: string;
+  company: string;
+  blog: string;
+  location: string;
+  email: null;
+  hireable: null;
+  bio: string;
+  twitter_username: null;
+  public_repos: number;
+  public_gists: number;
+  followers: number;
+  following: number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export const fetchGitHubInfo = async (username: string) => {
+  const response = await fetch(`https://api.github.com/users/${username}`);
+
+  if (response.status >= 200 && response.status <= 299) {
+    return (await response.json()) as GitHubInfoInterface;
+  } else {
+    throw new Error();
+  }
+};
